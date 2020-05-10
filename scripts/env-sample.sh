3a419e06493596e065bc3e55322ae222557cc65d
Claudio Fahey <claudio.fahey@dell.com>
2020-05-10 02:27:04 +0000

    Add script to copy a stream to AWS S3

--- scripts/env-sample.sh
+++ scripts/env-sample.sh
@@ -3,6 +3,11 @@
 export NAMESPACE=examples
 export MAVEN_USERNAME=desdp
 
+export S3_ENDPOINT=s3.amazonaws.com
+export S3_BUCKET=my-bucket-name
+export S3_ACCESS_KEY=xxx
+export S3_SECRET_KEY=xxx
+
 # Default set of charts to deploy.
 CHARTS=${CHARTS:-\
 stream-to-console-job \
