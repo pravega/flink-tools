1108787f7c5cb6b3aebfaab74350b732090b0257
Claudio Fahey <claudio.fahey@dell.com>
2020-06-06 03:11:50 +0000

    Static dedup

--- flink-tools/src/main/java/io/pravega/flinktools/StreamToFileJob.java
+++ flink-tools/src/main/java/io/pravega/flinktools/StreamToFileJob.java
@@ -10,15 +10,25 @@
  */
 package io.pravega.flinktools;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.pravega.client.stream.StreamCut;
 import io.pravega.connectors.flink.FlinkPravegaReader;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy;
+import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -69,11 +79,45 @@ public class StreamToFileJob extends AbstractJob {
                     .uid("pravega-reader")
                     .name("pravega-reader");
 
+            final ObjectMapper objectMapper = new ObjectMapper();
+            final DataStream<Tuple3<String,String,Long>> withDups = lines.flatMap(new FlatMapFunction<String, Tuple3<String,String,Long>>() {
+                @Override
+                public void flatMap(String s, Collector<Tuple3<String,String,Long>> collector) throws Exception {
+                    final JsonNode node = objectMapper.readTree(s);
+                    collector.collect(Tuple3.of(s, node.get("sensorId").asText(), node.get("timestamp").asLong()));
+                }
+            });
+            final DataStream<String> withoutDups = withDups
+                    .keyBy(t -> t.f1)
+                    .process(new KeyedProcessFunction<String, Tuple3<String, String, Long>, Tuple3<String, String, Long>>() {
+                        private ValueState<Long> maxCounterState;
+
+                        @Override
+                        public void open(Configuration parameters) throws Exception {
+                            maxCounterState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("maxCounter", Long.class));
+                        }
+
+                        @Override
+                        public void processElement(Tuple3<String, String, Long> value, Context ctx, Collector<Tuple3<String, String, Long>> out) throws Exception {
+                            final long counter = value.f2;
+                            final Long maxCounter = maxCounterState.value();
+                            if (maxCounter == null || maxCounter < counter) {
+                                maxCounterState.update(counter);
+                                out.collect(value);
+                            } else {
+                                log.info("Dropping record with decreasing counter {} and key {}", counter, ctx.getCurrentKey());
+                            }
+                        }
+                    })
+                    .map(t -> t.f0);
+            final DataStream<String> toOutput = withoutDups;
+            toOutput.printToErr();
+
             final StreamingFileSink<String> sink = StreamingFileSink
                     .forRowFormat(new Path(outputFilePath), new SimpleStringEncoder<String>())
                     .withRollingPolicy(OnCheckpointRollingPolicy.build())
                     .build();
-            lines.addSink(sink)
+            toOutput.addSink(sink)
                 .uid("file-sink")
                 .name("file-sink");
 
