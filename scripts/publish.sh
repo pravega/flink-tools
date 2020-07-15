df55e1a7f3fdcb1c6266b598a17ee3ba6798955e
chennav4 <chennv4@github.com>
2020-07-15 03:21:37 -0400

    moved NFS configurations to NFS job

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,7 +9,8 @@
 set -ex
 ROOT_DIR=$(dirname $0)/..
 source ${ROOT_DIR}/scripts/env-local.sh
-export MAVEN_PASSWORD=$(kubectl get secret keycloak-${MAVEN_USERNAME} -n nautilus-system -o jsonpath='{.data.password}' | base64 -d)
+#export MAVEN_PASSWORD=$(kubectl get secret keycloak-${MAVEN_USERNAME} -n nautilus-system -o jsonpath='{.data.password}' | base64 -d)
+export MAVEN_PASSWORD=password
 export MAVEN_URL=http://localhost:9092/maven2
 echo Starting kubectl port forward
 kubectl port-forward --namespace ${NAMESPACE} service/repo 9092:80 &
