fca22d055f241aa5d5453ed24db2c66239adb8f7
Claudio Fahey <claudio.fahey@dell.com>
2020-08-19 03:50:19 +0000

    Update README

--- scripts/hadoop-install.sh
+++ scripts/hadoop-install.sh
@@ -9,9 +9,15 @@
 set -ex
 ROOT_DIR=$(dirname $0)/..
 source ${ROOT_DIR}/scripts/env.sh
+: ${NAMESPACE?"You must export NAMESPACE"}
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm upgrade --install --timeout 600s --wait --debug \
     hadoop \
     --namespace ${NAMESPACE} \
     stable/hadoop \
+    --set yarn.nodeManager.replicas=0 \
     $@
+
+kubectl run --image danisla/hadoop:2.9.0 --generator=run-pod/v1 \
+    --rm --attach --restart=Never hadoop-$(uuidgen) -n ${NAMESPACE} -- \
+    hadoop fs -chmod 777 hdfs://hadoop-hadoop-hdfs-nn.examples.svc.cluster.local:9000/
